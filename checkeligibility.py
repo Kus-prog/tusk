ec0dc1a434bbdedda53b30b4254b2c41fdd6e47b
Kus-prog <70935502+Kus-prog@users.noreply.github.com>
2020-09-22 21:37:39 +0530

    Add files via upload

---
+++ checkeligibility.py
@@ -0,0 +1,9 @@
+def checkeligibility( age):
+    if(age>=18):
+        print("you are eligible for voting:")
+    else:
+        print("you are not eligible for vote:")
+
+checkeligibility(15)
+checkeligibility(25)
+
